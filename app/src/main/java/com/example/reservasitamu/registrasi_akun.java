e822d5fa4d1a1b848d4a7088adf9d63788abf4ce
vaniavianaa <vani2018rplrus2@gmail.com>
2020-01-17 11:00:51 +0700

    register

---
+++ app/src/main/java/com/example/reservasitamu/registrasi_akun.java
@@ -0,0 +1,14 @@
+package com.example.reservasitamu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrasi_akun extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi_akun);
+    }
+}
