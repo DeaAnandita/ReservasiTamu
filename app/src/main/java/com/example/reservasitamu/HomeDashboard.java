4446b89abaea92d5b3b24a843392cba314cf19d9
hasanwijaya <hasan@bitmessage.ch>
2020-01-21 11:26:03 +0700

    push

--- app/src/main/java/com/example/reservasitamu/HomeDashboard.java
+++ app/src/main/java/com/example/reservasitamu/HomeDashboard.java
@@ -1,14 +1,29 @@
 package com.example.reservasitamu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeDashboard extends AppCompatActivity {
+
+    CardView cvPaket1, cvPaket2, cvPaket3, cvPaket4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_dashboard);
+
+        cvPaket1 = findViewById(R.id.cvPaket1);
+
+        cvPaket1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeDashboard.this, Booking.class));
+            }
+        });
+
     }
 }
